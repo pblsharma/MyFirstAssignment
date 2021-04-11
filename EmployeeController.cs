20666d88e7bc56b01527a6201e4a5098628a9553
pblsharma <82312522+pblsharma@users.noreply.github.com>
2021-04-11 18:28:27 +0530

    Web API main files code

---
+++ EmployeeController.cs
@@ -0,0 +1,48 @@
+﻿using MyFirstAssessment.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MyFirstAssessment.Controllers
+{
+	public class EmployeeController : ApiController
+	{
+		[Route("api/Employee/GetEmployee")]
+		[HttpGet]
+		public List<clsEmployee> GetEmployee(employeeInput input)
+		{
+			List<clsEmployee> objList = new List<clsEmployee>();
+			objList  = FileterEmployee( input);
+			return objList;
+		}	
+		public List<clsEmployee> FileterEmployee(employeeInput input)
+		{
+			string userName = input.userName;
+			DateTime StartDate = input.startDate;
+			DateTime EndDate = input.endDate;
+			List<clsEmployee> objList = new List<clsEmployee>();
+			objList.Add(new clsEmployee { Age = 30, FirstName = "Parvesh", JobTitle = "Software Engg", EndDate = Convert.ToDateTime("01-Oct-2021"), LastName = "Kumar", StartDate = Convert.ToDateTime("01-Jan-2021") });
+			objList.Add(new clsEmployee { Age = 30, FirstName = "Sanjay", JobTitle = "Software Engg", EndDate = Convert.ToDateTime("01-Nov-2021"), LastName = "Arora", StartDate = Convert.ToDateTime("01-Feb-2021") });
+			objList.Add(new clsEmployee { Age = 30, FirstName = "Sumit", JobTitle = "Software Engg", EndDate = Convert.ToDateTime("01-Dec-2021"), LastName = "Sharma", StartDate = Convert.ToDateTime("01-Mar-2021") });
+			objList.Add(new clsEmployee { Age = 30, FirstName = "Ankit", JobTitle = "Software Engg", EndDate = Convert.ToDateTime("01-Jan-2022"), LastName = "Khanna", StartDate = Convert.ToDateTime("01-Apr-2021") });
+
+			var result =  objList.Where(x => x.FirstName == userName || x.LastName == userName).ToList<clsEmployee>();
+			if (!string.IsNullOrEmpty(StartDate.ToString()) && !string.IsNullOrEmpty(EndDate.ToString()))
+			{
+				return result.Where(x => x.StartDate >= StartDate && x.EndDate <= EndDate).ToList<clsEmployee>();
+			}
+			else if (!string.IsNullOrEmpty(StartDate.ToString()))
+			{
+				return result.Where(x => x.StartDate >= StartDate).ToList<clsEmployee>();
+			}
+			else
+			{
+				return result.Where(x => x.EndDate <= EndDate).ToList<clsEmployee>();
+			}
+		}
+	}
+	
+}
---
+++ clsEmployee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyFirstAssessment.Models
+{
+	public class clsEmployee
+	{
+		public string FirstName;
+		public string LastName;
+		public string JobTitle;
+		public int Age;
+		public DateTime StartDate;
+		public DateTime EndDate;
+	}
+	public class employeeInput
+	{
+		public string userName;
+		public DateTime startDate;
+		public DateTime endDate;		
+	}
+}
